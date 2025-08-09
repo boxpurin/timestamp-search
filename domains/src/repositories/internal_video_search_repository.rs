971f5a323ec4cc64e338638efdc8aacca63e011c
boxpurin <boxpurin@gmail.com>
2025-08-09 15:23:00 +0900

    domainsにテスト書くのでtokioをつける

--- domains/src/repositories/internal_video_search_repository.rs
+++ domains/src/repositories/internal_video_search_repository.rs
@@ -1,6 +1,7 @@
 use crate::entities::video::VideoEntity;
 use errors::AppResult;
 
+#[derive(Debug, PartialOrd, PartialEq, Clone)]
 pub struct SearchQuery {
     pub query: String,
 }
@@ -9,4 +10,24 @@ pub struct SearchQuery {
 #[async_trait::async_trait]
 pub trait InternalVideoSearchRepository {
     async fn search_videos_by_query(&self, query: &SearchQuery) -> AppResult<Vec<VideoEntity>>;
+}
+
+#[cfg(test)]
+mod unit_tests{
+    use crate::repositories::internal_video_search_repository::{InternalVideoSearchRepository, MockInternalVideoSearchRepository};
+    use super::*;
+    use mockall::predicate::eq;
+
+    #[tokio::test]
+    async fn mock_internal_video_search(){
+        let mut mock = MockInternalVideoSearchRepository::new();
+        let query = SearchQuery{ query : "query".to_string() };
+
+        mock.expect_search_videos_by_query()
+            .with(eq(query.clone()))
+            .returning(|_| Ok(vec![]));
+
+        let v = mock.search_videos_by_query(&query).await;
+        assert!(v.is_ok());
+    }
 }
