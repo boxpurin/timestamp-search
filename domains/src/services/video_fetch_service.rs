e54b86fa955d3e7ac428c4b20faf5235dacfa0c6
boxpurin <boxpurin@gmail.com>
2025-08-09 12:31:58 +0900

    取得してきたビデオの追加は別サービスにすることに

--- domains/src/services/video_fetch_service.rs
+++ domains/src/services/video_fetch_service.rs
@@ -2,17 +2,16 @@ use crate::repositories::external_video_repository::ExternalVideoRepository;
 use crate::repositories::internal_video_repository::InternalVideoRepository;
 use crate::value_objects::channel_id::ChannelId;
 use errors::AppResult;
+use crate::entities::video::VideoEntity;
 
-pub struct VideoFetchService<E: ExternalVideoRepository, I: InternalVideoRepository> {
+pub struct VideoFetchService<E: ExternalVideoRepository> {
     external_video_repository: E,
-    internal_video_repository: I,
 }
 
-impl<E: ExternalVideoRepository, I: InternalVideoRepository> VideoFetchService<E, I> {
-    pub fn new(external_video_repository: E, internal_video_repository: I) -> Self {
+impl<E: ExternalVideoRepository> VideoFetchService<E> {
+    pub fn new(external_video_repository: E) -> Self {
         VideoFetchService {
             external_video_repository,
-            internal_video_repository,
         }
     }
 
@@ -20,48 +19,23 @@ impl<E: ExternalVideoRepository, I: InternalVideoRepository> VideoFetchService<E
         &self,
         channel_id: &ChannelId,
         count: u32,
-    ) -> AppResult<()> {
+    ) -> AppResult<Vec<VideoEntity>> {
         let v = self
             .external_video_repository
             .fetch_recent_video_by_channel_id(channel_id, count)
             .await?;
 
         tracing::info!("Downloaded videos count {}", v.len());
-        for video in &v {
-            if !self
-                .internal_video_repository
-                .find_video_entity_by_id(&video.id)
-                .await?
-            {
-                self.internal_video_repository
-                    .add_video_entity(video)
-                    .await?;
-            }
-        }
-
-        Ok(())
+        Ok(v)
     }
 
-    pub async fn fetch_all_videos_by_channel_id(&self, channel_id: &ChannelId) -> AppResult<()> {
+    pub async fn fetch_all_videos_by_channel_id(&self, channel_id: &ChannelId) -> AppResult<Vec<VideoEntity>> {
         let v = self
             .external_video_repository
             .fetch_all_videos_by_channel_id(channel_id)
             .await?;
 
         tracing::info!("Downloaded videos count {}", v.len());
-        for video in &v {
-            let ret = self
-                .internal_video_repository
-                .find_video_entity_by_id(&video.id)
-                .await?;
-            if !ret
-            {
-                self.internal_video_repository
-                    .add_video_entity(video)
-                    .await?;
-            }
-        }
-
-        Ok(())
+        Ok(v)
     }
 }
