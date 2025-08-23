7e66bf39e2273935bc6970ad1c913595255cb205
boxpurin <boxpurin@gmail.com>
2025-08-23 13:44:13 +0900

    API Server

--- applications/server/presentations/api/src/lib.rs
+++ applications/server/presentations/api/src/lib.rs
@@ -1,2 +1,10 @@
+pub (crate)mod api;
+pub (crate)mod route;
+pub (crate)mod request;
+pub (crate)mod response;
+pub (crate)mod handle;
+pub (crate)mod app_state;
+pub mod service;
+
 #[cfg(test)]
 mod unit_tests {}
