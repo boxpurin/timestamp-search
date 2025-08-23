723d03271d76e7724885f7625cc8d4677a9f8f97
boxpurin <boxpurin@gmail.com>
2025-08-23 13:43:07 +0900

    期待される値をYoutubeに合わせる

--- domains/src/value_objects/height.rs
+++ domains/src/value_objects/height.rs
@@ -1,11 +1,26 @@
 use errors::{AppError, AppResult};
-types::impl_numeric_value!(Height, u32);
-impl Height {
+types::impl_numeric_value!(VideoThumbnailHeight, u32);
+
+/// Expected Resolution Height when fetch from Youtube Data API v3
+///
+/// https://developers.google.com/youtube/v3/docs/thumbnails
+const STANDARD_HEIGHTS: &[u32] = &[
+    90,    // default
+    240,    // medium
+    360,    // high
+    480,    // standard
+    720,   // maxres
+];
+
+impl VideoThumbnailHeight {
     pub fn new(height: u32) -> AppResult<Self> {
-        if height == 0 {
-            return Err(AppError::InvalidInput("Height cannot be zero".to_string()));
+        if !STANDARD_HEIGHTS.contains(&height) {
+            return Err(AppError::InvalidInput(
+                format!("Invalid standard height: {} , Expected one of {:?}",height,STANDARD_HEIGHTS)
+            ));
         }
-        Ok(Height(height))
+
+        Ok(VideoThumbnailHeight(height))
     }
 }
 
@@ -15,20 +30,23 @@ mod unit_tests {
 
     #[rstest::rstest]
     #[test]
-    #[case(vec![180, 240, 360, 480, 720])]
+    #[case(vec![90, 240, 360, 480, 720])]
     fn valid_height(#[case] heights: Vec<u32>) {
         for height in heights {
-            assert!(Height::new(height).is_ok());
+            assert!(VideoThumbnailHeight::new(height).is_ok());
 
-            let h = Height::new(height).unwrap();
+            let h = VideoThumbnailHeight::new(height).unwrap();
             assert_eq!(h, height);
         }
     }
 
     #[rstest::rstest]
     #[test]
-    #[case(0)]
-    fn invalid_height(#[case] height: usize) {
-        assert!(Height::new(0).is_err());
+    #[case(vec![0, 120, 640, 1280, 1920])]
+    fn invalid_height(#[case] heights: Vec<u32>)
+    {
+        for height in heights {
+            assert!(VideoThumbnailHeight::new(height).is_err());
+        }
     }
 }
--- domains/src/value_objects/width.rs
+++ domains/src/value_objects/width.rs
@@ -1,8 +1,56 @@
 use errors::AppResult;
-types::impl_numeric_value!(Width, u32);
+use errors::AppError::InvalidInput;
 
-impl Width {
+types::impl_numeric_value!(VideoThumbnailWidth, u32);
+
+/// Expected Resolution Width when fetch from Youtube Data API v3
+///
+/// https://developers.google.com/youtube/v3/docs/thumbnails
+const STANDARD_WIDTHS: &[u32] = &[
+    120,    // default
+    320,    // medium
+    480,    // high
+    640,    // standard
+    1280,   // maxres
+];
+
+impl VideoThumbnailWidth {
     pub fn new(width: u32) -> AppResult<Self> {
-        Ok(Width(width))
+        if !STANDARD_WIDTHS.contains(&width) {
+            Err(InvalidInput(
+                format!("Invalid standard width: {} , Expected one of {:?}",
+                        width,
+                        STANDARD_WIDTHS))
+            )?;
+        }
+
+        Ok(VideoThumbnailWidth(width))
     }
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use super::*;
+
+    #[rstest::rstest]
+    #[test]
+    #[case(vec![120, 320, 480, 640, 1280])]
+    fn valid_width(#[case] widths: Vec<u32>) {
+        for w in widths {
+            assert!(VideoThumbnailWidth::new(w).is_ok());
+
+            let width = VideoThumbnailWidth::new(w).unwrap();
+            assert_eq!(width, width);
+        }
+    }
+
+    #[rstest::rstest]
+    #[test]
+    #[case(vec![0, 240, 360, 1600, 777, 1920])]
+    fn invalid_width(#[case] widths: Vec<u32>) {
+        for w in widths {
+            assert!(VideoThumbnailWidth::new(w).is_err());
+        }
+    }
+
 }
