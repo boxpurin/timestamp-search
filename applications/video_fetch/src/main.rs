3fc1eec385befd11a4d9a22be5f7b767cdd499ee
boxpurin <boxpurin@gmail.com>
2025-08-30 07:38:37 +0900

    feat ビデオ取得先をローカルと外部サービスを選べるように。出力先もデータベースやローカルファイル化を選べるように

--- applications/video_fetch/src/main.rs
+++ applications/video_fetch/src/main.rs
@@ -12,33 +12,79 @@ use meilisearch::repositories::{
 use youtube::repositories::youtube_video::create_youtube_video_repository;
 use std::sync::Arc;
 use errors::{AppError, AppResult};
+use clap::Parser;
+use std::fs::File;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(long, default_value = "false")]
+    pub in_external: bool,
+
+    #[arg(short, long, conflicts_with = "in_external")]
+    pub in_json: Option<String>,
+
+    #[arg(long, default_value = "false")]
+    pub out_internal: bool,
+
+    #[arg(short, long, conflicts_with = "out_internal")]
+    pub out_json: Option<String>,
+}
 
 #[tokio::main]
 async fn main() -> AppResult<()>{
     tracing_subscriber::fmt::init();
 
-    let ext_repo = Arc::new(create_youtube_video_repository().await);
-    let int_repo = Arc::new(create_video_crud_repository());
-    let tss_repo = Arc::new(create_timestamp_crud_repository());
+    let args = Args::parse();
+    println!("{:?}", args);
+    tracing::info!("{:?}", args);
 
-    let video_fetch_service = VideoFetchService::new(ext_repo.clone());
-    let parser = TimeStampParserService::new();
-    let video_indexing = VideoIndexingService::new(int_repo.clone());
-    let ts_indexing = TimeStampIndexingService::new(tss_repo.clone(), int_repo.clone());
+    let videos = if args.in_external {
+        tracing::info!("Load video from external service.");
+        let channel_id = std::env::var("TSSEARCH_TARGET_CHANNEL_ID").expect("TSSEARCH_TARGET_CHANNEL_ID");
+        let ext_repo = Arc::new(create_youtube_video_repository().await);
+        let video_fetch_service = VideoFetchService::new(ext_repo.clone());
+        let channel_id = ChannelId::new(&channel_id)?;
 
-    let channel_id = ChannelId::new("TSSEARCH_TARGET_CHANNEL_ID").expect("Invalid channel ID");
-    // Replace with the actual channel ID you want to fetch videos for
-    let vs = video_fetch_service
-        .fetch_all_videos_by_channel_id(&channel_id)
-        .await?;
+        let videos = video_fetch_service.fetch_all_videos_by_channel_id(&channel_id).await?;
 
-    if let Err(_) = video_indexing.add_or_update_video_entities(&vs).await{
-        return Err(AppError::InvalidInput("Failed to add video entities".to_string()));
-    }
+        videos
+    } else {
+        tracing::info!("Load video entity from local json file.");
+        let mut videos = Vec::new();
+        if let Some(in_json) = args.in_json {
+            let file = File::open(in_json).expect("Unable to open file");
+            videos = serde_json::from_reader(file).expect("Invalid JSON file");
+        } else {
+            panic!("Input json file is not set.");
+        }
+        videos
+    };
 
-    for v in vs {
-        if let Ok(tss) = parser.parse_video(&v){
-            ts_indexing.add_or_update_timestamps(&v, tss.as_slice()).await?;
+    if args.out_internal {
+        tracing::info!("Output video and timestamps from internal meilisearch.");
+        let int_repo = Arc::new(create_video_crud_repository());
+        let tss_repo = Arc::new(create_timestamp_crud_repository());
+
+        let parser = TimeStampParserService::new();
+        let video_indexing = VideoIndexingService::new(int_repo.clone());
+        let ts_indexing = TimeStampIndexingService::new(tss_repo.clone(), int_repo.clone());
+
+        if let Err(_) = video_indexing.add_or_update_video_entities(&videos).await{
+            return Err(AppError::InvalidInput("Failed to add video entities".to_string()));
+        }
+
+        for v in videos {
+            if let Ok(tss) = parser.parse_video(&v){
+                ts_indexing.add_or_update_timestamps(&v, tss.as_slice()).await?;
+            }
+        }
+    } else {
+        tracing::info!("Output video to json file.");
+        if let Some(file) = args.out_json {
+            let file = File::create(file).expect("Unable to open file");
+            serde_json::to_writer_pretty(file, &videos).expect("Unable to write to file");
+        } else {
+            println!("Output to json file is not set.");
         }
     }
 
