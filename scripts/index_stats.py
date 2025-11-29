427d5f0246dd479730b6d7e9cd96f987d16b09e4
boxpurin <boxpurin@gmail.com>
2025-11-30 04:25:11 +0900

    APIDOG使って見るようにしたのでいらなくなった

--- scripts/index_stats.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-from client import MeilisearchIndexSetup
-
-index: str = argv[1]
-client = MeilisearchIndexSetup()
-if client.health_check():
-    stats = client.get_index_stats(index)
-    for k, v in stats.items():
-        print(k, v)
