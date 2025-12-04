d87f6b99df3951670bbc765f4a92cb976a96d38a
boxpurin <boxpurin@gmail.com>
2025-12-05 04:27:49 +0900

    なし

--- service-backend/services/restful_server/src/api/middleware.rs
+++ service-backend/services/restful_server/src/api/middleware.rs
@@ -7,6 +7,7 @@ use axum::{
 };
 use axum::http::header;
 use errors::{AppResult, AppError};
+use crate::api::config::SERVER_CONFIG;
 
 pub async fn access_log_console(req: Request, next: Next) -> Result<impl IntoResponse, Response> {
     tracing::info!("Request method : {} , uri : {} ", req.method(), req.uri());
@@ -17,7 +18,10 @@ pub async fn access_log_console(req: Request, next: Next) -> Result<impl IntoRes
     Ok(res)
 }
 
-/// API全体のRateLimitterのトークンの消費を実行する0
+///
+/// API全体のRateLimitterのトークンの消費を実行する
+/// 枯渇している場合はTooManyRequestを返す
+///
 pub async fn use_backet(
     State(state): State<AppState>,
     req: Request,
@@ -35,19 +39,4 @@ pub async fn use_backet(
 
     let res = next.run(req).await;
     Ok(res)
-}
-
-pub async fn check_auth(req: Request, next: Next) -> AppResult<Response> {
-    let headers = req.headers();
-    let provided = headers
-        .get(header::AUTHORIZATION)
-        .ok_or(AppError::Unauthorized)?;
-    let provided = provided.to_str().map_err(|_| AppError::Unauthorized)?;
-    
-    let expected = "";
-    if provided != expected {
-        Ok(next.run(req).await)
-    } else {
-        Err(AppError::Unauthorized)
-    }
 }
